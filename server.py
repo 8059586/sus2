770e59e8fc0135798b44779e16dcca2fd54ace02
8059586 <70647848+8059586@users.noreply.github.com>
2022-07-02 20:37:13 -0500

    Update server.py

--- server.py
+++ server.py
@@ -30,11 +30,25 @@ questions = [ " What is the Italian word for PIE? \n a.Mozarella\n b.Pasty\n c.P
              " Who gifted the Statue of Libery to the US? \n a.Brazil\n b.France\n c.Wales\n d.Germany",
              " Which planet is closest to the sun? \n a.Mercury\n b.Pluto\n c.Earth\n d.Venus" ]
 
+answers = [ 'd', 'a', 'b', 'a', 'a', 'a', 'b', 'a', 'c', 'b', 'd', 'd',
+           'c', 'a', 'b', 'a' ]
+
 print("Server has started...")
 
 def remove(connection):
     if connection in list_of_clients:
         list_of_clients.remove
+
+def get_random_question_answer(conn):
+  random_index = random.randint(0, len(questions) -1)
+  random_question = questions[random_index]
+  random_answer = answers[random_index]
+  conn.send(random_question.encode('utf-8'))
+  return random_index, random_question, random_answer
+
+def remove_question(index):
+  questions.pop(index)
+  answers.pop(index)
 
 def clientthread(conn):
     score = 0
@@ -46,14 +60,15 @@ def clientthread(conn):
         try:
             message = conn.recv(2048).decode('utf-8')
             if message:
-                print ("<" + addr[0] + "> " + message)
-
-                message_to_send = "<" + addr[0] + "> " + message
-                broadcast(message_to_send, conn)
+              if message.lower() == answer:
+                 score += 1
+                 conn.send("Bravo! Your score is {score}\n\n".encode('utf-8'))
+              else:
+                 conn.send("Incorrect answer! Better luck next time!\n\n".encode('utf-8'))
             else:
-                remove(conn)
+              remove(conn)
         except:
-            continue
+          continue
 
 while True:
     conn, addr = server.accept()
