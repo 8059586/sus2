855cd87b61186996cd5b27a5bbc8bcf1cd954757
8059586 <70647848+8059586@users.noreply.github.com>
2022-07-02 20:08:23 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 from threading import Thread
+import random
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -11,22 +12,36 @@ server.listen()
 
 list_of_clients = []
 
-print("Server has started...")
+questions = [ " What is the Italian word for PIE? \n a.Mozarella\n b.Pasty\n c.Patty\n d.Pizza",
+             " Water boils at 212 Units at which scale? \n a.Fahrenheit\n b.Celsius\n c.Rankine\n d.Kelvin",
+             " Which sea creature has three hearts? \n a.Dolphin\n b.Octopus\n c.Walrus\n d.Seal",
+             " Who was the character famous in our childhood rhymes associated with a lamb? \n a.Mary\n b.Jack\n c.Johnny\n d.Mukesh",
+             " How many bones does an adult human have? \n a.206\n b.208\n c.201\n d.196",
+             " How many wonders are there in the world? \n a.7\n b.8\n c.10\n d.4",
+             " What element does not exist? \n a.Xf\n b.Re\n c.Si\n d.Pa",
+             " How many states are there in India? \n a.24\n b.29\n c.30\n d.31",
+             " Who invented the telephone? \n a.A.G Bell\n b.John Wick\n c.Thomas Edison\n d.G Marconi",
+             " Who is Loki? \n a.God of Thunder\n b.God of Dwarves\n c.God of Mischief\n d.God of Gods",
+             " Who was the first Indian female astronaut ? \n a.Sunita Williams\n b.Kalpana Chawla\n c.None of them\n d.Both of them ",
+             " What is the smallest continent? \n a.Asia\n b.Antarctic\n c.Africa\n d.Australia",
+             " The beaver is the national embelem of which country? \n a.Zimbabwe\n b.Iceland\n c.Argentina\n d.Canada",
+             " How many players are on the field in baseball? \n a.6\n b.7\n c.9\n d.8",
+             " Hg stands for? \n a.Mercury\n b.Hulgerium\n c.Argenine\n d.Halfnium",
+             " Who gifted the Statue of Libery to the US? \n a.Brazil\n b.France\n c.Wales\n d.Germany",
+             " Which planet is closest to the sun? \n a.Mercury\n b.Pluto\n c.Earth\n d.Venus" ]
 
-def broadcast(message, connection):
-    for clients in list_of_clients:
-        if clients != connection:
-            try:
-                clients.send(message.encode("utf-8"))
-            except:
-                remove(clients)
+print("Server has started...")
 
 def remove(connection):
     if connection in list_of_clients:
         list_of_clients.remove
 
-def clientthread(conn, addr):
-    conn.send("Welcome to this chatroom!".encode('utf-8'))
+def clientthread(conn):
+    score = 0
+    conn.send("Welcome to this quiz game!".encode('utf-8'))
+    conn.send("You will recieve a question. The answer to that question should be of a, b, c, or d.".encode('utf-8'))
+    conn.send("Good luck!\n\n".encode('utf-8'))
+    index, question, answer = get_random_question_answer(conn)
     while True:
         try:
             message = conn.recv(2048).decode('utf-8')
